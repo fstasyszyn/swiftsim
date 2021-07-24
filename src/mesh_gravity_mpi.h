a08912e55a8a86a3578babddda3d5a6307c9de72
Matthieu Schaller <matthieu.schaller@durham.ac.uk>
2021-07-24 23:39:16 +0200

    Move the new mesh-gravity sorting functions to a separate file

--- src/mesh_gravity_mpi.h
+++ src/mesh_gravity_mpi.h
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  ******************************************************************************/
-
 #ifndef SWIFT_MESH_GRAVITY_MPI_H
 #define SWIFT_MESH_GRAVITY_MPI_H
 
